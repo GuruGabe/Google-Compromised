a8ca0b93051379a7afdb523539f1d9b5228c0b00
Gabriel Clifton <46380574+GuruGabe@users.noreply.github.com>
2022-02-23 17:29:41 -0600

    Create All Accounts PS1 for Email-all-out-of-country.bat

---
+++ All Accounts PS1 for Email-all-out-of-country.bat
@@ -0,0 +1,35 @@
+$csv = import-csv "C:\Temp\All_logins3.csv"
+
+$report = 
+foreach($row in $csv){
+    $apidata     = Invoke-RestMethod "http://api.geoiplookup.net/?query=$($row.IP_Address)"
+    $city        = $apidata.ip.results.result.city
+    $countryname = $apidata.ip.results.result.countryname
+    
+    [pscustomobject]@{
+       "Login Result" = $row.login_result
+       "email address" = $row.email_address
+       Time = $row.time
+       "IP Address" = $row.IP_Address
+       Suspicious = $row.suspicious
+       Type = $row.type
+       City = $city
+       "Country Name" = $countryname
+    }
+}
+$report | Export-Csv "C:\Temp\All_logins4.csv" -NoTypeInformation -Encoding:UTF8
+
+
+$data = foreach($line in Get-Content "C:\Temp\All_logins4.csv")
+{
+    if($line -like '*Fort Stockton*')
+    {
+
+    }
+    else
+    {
+        $line
+    }
+
+}
+$data | Set-Content "C:\Temp\All_logins4.csv" -Force
